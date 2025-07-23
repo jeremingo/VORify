d14eb5e511ff060a4785e44c41a659b2f5abec10
jeremingo <58335558+jeremingo@users.noreply.github.com>
2025-07-23 20:20:54 +0300

    gh-64 Add Thread that Prints Stations In JSON Format

--- main/main.cpp
+++ main/main.cpp
@@ -12,6 +12,7 @@ std::string generateRMC(double lat, double lon);
 std::vector<Entry> getStationsWithinRange(const double lat, const double lon, const int range);
 std::optional<double> calculateBearing(double frequency);
 std::optional<Location> intersection(const std::vector<Entry>& entries);
+std::string entriesToJson(const std::vector<Entry>& entries);
 
 FILE* startBluetoothServer() {
     FILE* pipe = popen("../bluetooth-server/bluetooth-server", "w");
@@ -30,6 +31,16 @@ void sendToBluetooth(FILE* pipe, const std::string& data) {
 }
 
 std::mutex entryMutex;
+
+void startJSONOutput(std::vector<Entry>& entries, bool& running) {
+  std::thread([&entries, &running]() {
+    while (running) {
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+      std::cout << entriesToJson(entries) << std::endl;
+    }
+  }).detach();
+}
+
 
 void startBearingUpdater(std::vector<Entry>& entries, bool& running) {
     std::thread([&entries, &running]() {
@@ -61,6 +72,7 @@ int main() {
     std::vector<Entry> entries = getStationsWithinRange(35.0, 128.0, 400);
     bool running = true;
 
+    startJSONOutput(entries, running);
     startBearingUpdater(entries, running);
 
     while (running) {
